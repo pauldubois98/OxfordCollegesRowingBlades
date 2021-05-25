343326e8ffa0e8ad2a2b0a33326c6a1c71b2fcb0
pauldubois98 <pauldubois98@gmail.com>
2021-05-25 02:14:45 +0200

    added giveup button

--- colleges_to_blades.js
+++ colleges_to_blades.js
@@ -5,7 +5,7 @@ var colleges = [
     "Magdalen", "St. Anne's", "Trinity", "Exeter", "Mansfield", "St. Antony's", "University", "Green Templeton", "Merton",
     "St. Benet's Hall", "Wadham", "Hertford", "New College", "St. Caterine's", "Wolfson", "Hertford M1", "Oriel", "St. Caterine's M1",
     "Wolfson M2", "Hertford W1", "Oriel M1", "St. Caterine's W1", "Worcester Men", "Jesus", "Oriel W1", "St. Edmond Hall", 
-    "Worcester Women", "Keble", "Osler", "St. Hilda's", //"St. Peters Bis", 
+    "Worcester Women", "Keble", "Osler", "St. Hilda's", "St. Peters Bis", 
 ]
 function fileName(collegeName){
     return collegeName.replace(' ','').replace(' ','').replace('.','').replace("'","")
@@ -30,6 +30,7 @@ document.getElementById("blades").innerHTML = s;
 // global variables
 var blades = document.getElementById("blades").children
 var college = document.getElementById("college")
+var give_up = document.getElementById("give_up")
 var tries = 0;
 var wrongs = 0;
 
@@ -47,20 +48,33 @@ function drawCollege(){
 function noFrame(){
     for(var i=0; i<blades.length; i++){
         var element = blades[i];
-        element.style.borderColor = "white"
+        element.style.borderColor = "white";
     }
+}
+function giveUp(){
+    for(var i=0; i<blades.length; i++){
+        var element = blades[i];
+        var selected = element.getAttribute("src").split('/')[1].split('.')[0]
+        if(selected==fileName(college.innerText)){
+            element.style.borderColor = "black";
+        }
+        else{
+            element.style.borderColor = "white";
+        }
+    }
+    setTimeout(drawCollege, 1000)
 }
 //click listeners
 for(var i=0; i<blades.length; i++){
     var element = blades[i];
     element.addEventListener("click", function(e){
         noFrame()
-        e.target.style.borderColor = "black"
+        e.target.style.borderColor = "black";
         var selected = e.target.getAttribute("src").split('/')[1].split('.')[0]
         //console.log(selected)
         if(selected==fileName(college.innerText)){
             //console.log('right')
-            e.target.style.borderColor = "green"
+            e.target.style.borderColor = "green";
             if(tries===0){
                 const index = colleges.indexOf(college.innerText);
                 colleges.splice(index, 1);
@@ -69,7 +83,7 @@ for(var i=0; i<blades.length; i++){
         }
         else{
             //console.log('wrong')
-            e.target.style.borderColor = "red"
+            e.target.style.borderColor = "red";
             tries += 1;
             wrongs += 1;
         }
